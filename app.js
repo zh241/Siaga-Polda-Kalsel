14512b03cddec3c446d2a4ab5e712f3edfe5fa48
zh241 <zh241>
2026-07-12 22:46:38 +0800

    Remove duplicate makeElementDraggable function declaration to fix syntax error

--- app.js
+++ app.js
@@ -5180,43 +5180,6 @@ function closePeerConnection(uid, shouldStopStreamOnMobile = false) {
     }
 }
 
-function makeElementDraggable(elmnt, header) {
-    let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-    if (header) {
-        header.onmousedown = dragMouseDown;
-    } else {
-        elmnt.onmousedown = dragMouseDown;
-    }
-
-    function dragMouseDown(e) {
-        e = e || window.event;
-        // Don't drag if clicking buttons inside header
-        if (e.target.tagName === 'BUTTON' || e.target.closest('button')) return;
-        e.preventDefault();
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-        document.onmouseup = closeDragElement;
-        document.onmousemove = elementDrag;
-    }
-
-    function elementDrag(e) {
-        e = e || window.event;
-        e.preventDefault();
-        pos1 = pos3 - e.clientX;
-        pos2 = pos4 - e.clientY;
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-        elmnt.style.bottom = 'auto';
-        elmnt.style.right = 'auto';
-    }
-
-    function closeDragElement() {
-        document.onmouseup = null;
-        document.onmousemove = null;
-    }
-}
 
 // ============================================================
 // SEARCH LOCATION ON TACTICAL MAP (NOMINATIM API)
